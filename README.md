65a1238048174b49f2d4861986b943e734d45083
NestDima <dmitrii.nesterov.basic@gmail.com>
2023-01-27 00:25:11 +0300

    diassembled into files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cpp-search-server
 Финальный проект: поисковый сервер
+
+Компилляция на g++: 
+
+g++ -c document.cpp main.cpp read_input_functions.cpp request_queue.cpp search_server.cpp string_processing.cpp -std=c++1z
+
+g++ -o prog document.o main.o read_input_functions.o request_queue.o search_server.o string_processing.o
---
+++ search-server/paginator.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+#include "document.h"
+
+template<typename Iterator>
+    class IteratorRange {
+    public:
+        IteratorRange(const Iterator begin, const Iterator end)
+        : page_(begin, end)
+        {}
+        auto begin() const {
+            return page_.first;
+        }
+        auto end() const {
+            return page_.second;
+        }
+        size_t size() const {
+            return (distance(begin, end - 1));
+        }
+
+    private:
+        std::pair<Iterator, Iterator> page_;
+    };
+
+template<typename Iterator>
+std::ostream& operator <<(std::ostream& os, const IteratorRange<Iterator>& page ) {
+    for (auto it = page.begin(); it != page.end(); ++it) {
+        os << *it;
+    }
+    return os;
+}
+
+template <typename Iterator>
+    class Paginator {
+    public:
+        Paginator(Iterator begin, Iterator end, int size) : page_size_(size){
+            Iterator begining = begin;
+            Iterator ending = begin;
+            while(distance(ending, end) > size) {
+                advance(ending, size);
+                pages_.push_back(IteratorRange(begining, ending));
+                begining = ending;
+            }
+            if (begining < end) {
+                pages_.push_back(IteratorRange(ending, end));
+            }
+        }
+
+    auto begin() const {
+        return pages_.begin();
+    }
+    auto end() const {
+        return pages_.end();
+    }
+    int size() const {
+        return page_size_;
+    }
+
+    private:
+        int page_size_;
+        std::vector<IteratorRange<Iterator>> pages_;
+};
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
